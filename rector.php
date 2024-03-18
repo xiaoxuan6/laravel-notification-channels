270f668ef56e37f5fce669170996b766d4cdad05
xiaoxuan6 <1527736751@qq.com>
2024-03-18 08:05:50 +0800

    feat: Add support laravel 11

--- rector.php
+++ rector.php
@@ -1,10 +1,16 @@
 <?php
 
 declare(strict_types=1);
-
+/**
+ * This file is part of james.xue/laravel-notification-channels.
+ *
+ * (c) xiaoxuan6 <1527736751@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\{LevelSetList, SetList};
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
